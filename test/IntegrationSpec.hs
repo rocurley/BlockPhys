0eae85e2bcbe807637303e087256deec257410cb
Roger Curley <rocurley@gmail.com>
2016-05-30 17:11:14 -0400

    Tests work for new collision checker (although they fail)

--- test/IntegrationSpec.hs
+++ test/IntegrationSpec.hs
@@ -24,6 +24,8 @@ import Math.Polynomial hiding (x)
 
 import Graphics.Gloss.Interface.Pure.Game
 
+import Debug.Trace
+
 main :: IO ()
 main = hspec spec
 
@@ -42,22 +44,44 @@ instance Arbitrary FakeInput where
     dt <- abs <$> arbitrary
     return $ FakeInput event dt
 
+data Simulation = Simulation World [FakeInput] Time
+instance Arbitrary Simulation where
+  arbitrary = let
+    initialWorld = addJail $ emptyWorld $ Player $  Grounded (SupPos (0,-3) 0) 0 Nothing
+    in Simulation initialWorld <$> arbitrary <*> pure 0
+  --The shrinks don't work fully because the trailing shrinks are needed to time evolve to the problem.
+  --Add a time to the simulation type, where that time is evolved to after all the keypresses.
+  --When shrinking from the right, add times to that time.
+  --Also subject the end time to shrinking.
+  shrink (Simulation world [] t) = Simulation world [] <$> shrink t
+  shrink (Simulation world (i:is) t) =
+    [ Simulation (stepInput i world) is t
+    , let
+        Just (inputs, FakeInput _ dt) = unsnoc $ i:is
+        in Simulation world inputs $ t + dt
+    ] ++ (Simulation world (i:is) <$> shrink t)
+instance Show Simulation where
+  show (Simulation world inputs t) = let
+    worldStr = "addJail $ emptyWorld $ " ++ show (world^.player)
+    in "Simulation (" ++ worldStr ++ ") " ++ show inputs ++ show t
+
 stepInput :: FakeInput -> World -> World
 stepInput (FakeInput event dt) = handleEvent event . stepWorld dt
+
+playerInJail :: World -> Bool
+playerInJail world = let
+  (x,y) = world^.player.playerMovement.movLoc
+  in (abs x < 3) && (abs y < 3)
 
 spec :: Spec
 spec = do
   describe "Collisions" $ do
-    it "Players should stay in a box with random initial trajectory" $ within (10^5) $ property $ \ plr t -> let
+    it "Players should stay in a box with random initial trajectory" $ property $ \ plr t -> let
       world = addJail $ emptyWorld plr
-      newWorld = stepWorld (abs t) world
-      (x,y) = newWorld^.player.playerMovement.movLoc
-      in (abs x < 3) && (abs y < 3)
-    it "Players should stay in a box with button mashing" $ within (10^5) $ property $ \ keypresses -> let
-      world = addJail $ emptyWorld $ Player $  Grounded (SupPos (0,-3) 0) 0 Nothing
-      newWorld = foldr stepInput world (keypresses :: [FakeInput])
-      (x,y) = newWorld^.player.playerMovement.movLoc
-      in (abs x < 3) && (abs y < 3)
+      in playerInJail $ stepWorld (abs t) world
+    it "Players should stay in a box with button mashing" $ within (10^5) $ property $
+      \ (Simulation world keypresses t)-> playerInJail world ==> let
+      in playerInJail $ stepWorld t $ foldr stepInput world (keypresses :: [FakeInput])
     it "In this specific case, a collision should happen on the left side of the wall" $ let
       traj = PolyTrajectory (poly BE [7.5,0.0,2.3]) (poly BE [-2.1])
       dir = fmap (\ (Collision _ _ _ dir) -> dir) $ minimumByMay (comparing (\ (Collision t _ _ _) -> t)) $
